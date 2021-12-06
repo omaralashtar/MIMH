622376780ccef995f7cec89d242c65496cb3a8f6
omaralashtar <ashtrr55@gmail.com>
2021-12-06 22:01:24 +0200

    add print

--- app/src/main/java/com/MadeInMyHome/MainActivity.java
+++ app/src/main/java/com/MadeInMyHome/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("hi6");
+        System.out.println("hi6");
     }
 }
